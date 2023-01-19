35dfbc809e7170e58e1d2d446beaf332fade0f8d
awinters81 <alexandria.winters81@gmail.com>
2023-01-19 08:15:07 -0500

    created package.json and beginning of seeds

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+-- Let me start with the employees, so I have something to test...
+
+use employees;
+
+-- 
+INSERT INTO employee
+    (first_name, last_name, position, manager)
+VALUES
+    ('Melody', 'Chance'),
+    ('David', 'Perez'),
+    ('Daisy', 'Summers'),
+    ('Autumn', 'Park',);
+  
